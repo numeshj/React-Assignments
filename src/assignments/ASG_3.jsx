b39a2903d0b58021118055c576398935ed3423cc
numeshj <numeshjayamanne@gmail.com>
2025-06-16 13:43:20 +0530

    Assginment#3-1

--- src/assignments/ASG_3.jsx
+++ src/assignments/ASG_3.jsx
@@ -1,45 +1,53 @@
-import BackToHome from '../component/BackToHome'
+import BackToHome from "../component/BackToHome";
 import "../assignments/AGS_3.css";
-import { useState } from 'react';
- 
-export default function ASG_3() {
+import { useState } from "react";
 
-  const [numbers, setNumbers] = useState ([])
-  const [inputValue, setInputValue] = useState('')
-  const [average, setAverage] = useState(0)
-  const [total, setTotal] = useState(0)
+export default function ASG_3() {
+  const [numbers, setNumbers] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [average, setAverage] = useState(0);
+  const [total, setTotal] = useState(0);
 
   const addNumber = () => {
-    const newNumber = parseFloat(inputValue)
+    const newNumber = parseFloat(inputValue);
     if (!isNaN(newNumber)) {
-      const updatedNumbers = [...numbers, newNumber]
-      setNumbers(updatedNumbers)
-      console.log(updatedNumbers)
-      
+      const updatedNumbers = [...numbers, newNumber];
+      setNumbers(updatedNumbers);
+      console.log(updatedNumbers);
+
       const sum = updatedNumbers.reduce((acc, curr) => acc + curr, 0);
       setTotal(sum);
       setAverage(sum / updatedNumbers.length);
-      setInputValue('');
+      setInputValue("");
     }
-  }
+  };
 
   return (
     <>
-      <BackToHome/>
+      <BackToHome />
       <h1 className="assignment-title">Assignment-3</h1>
-      <hr/>
-      <br/>
+      <hr />
+      <br />
       <label>Enter the Number : </label>
-      <input className="input-number" type='number' value={inputValue} onChange={(e)=> setInputValue(e.target.value)}/>
+      <input
+        className="input-number"
+        type="number"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
       <button onClick={addNumber}>Add</button>
-      <br/>
-      <br/>
-      <label> All number entered :  {numbers.join(',')}</label>
-      <br/>
-      <br/>
-      <label> TOtal   :{total}</label>
-      <br/>
-      <label> Average :  {average.toFixed(2)}</label>
+      <br />
+      <br />
+      <ol>
+        {numbers.map((num, index) => (
+          <li key={index}>{num}</li>
+        ))}
+      </ol>
+      <br />
+      <br />
+      <label> TOtal :{total}</label>
+      <br />
+      <label> Average : {average.toFixed(2)}</label>
     </>
-  )
+  );
 }
