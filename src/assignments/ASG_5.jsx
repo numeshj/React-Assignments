6560cb69f6cfd950d8045e2814e9bc3cc5320e1d
numeshj <numeshjayamanne@gmail.com>
2025-06-17 11:39:46 +0530

    Assignment #5

--- src/assignments/ASG_5.jsx
+++ src/assignments/ASG_5.jsx
@@ -1,15 +1,80 @@
 import BackToHome from "../component/BackToHome";
-import "../assignments/AGS_3.css";
-import { useState } from "react";
+import "../assignments/AGS_4.css";
+import { useState, useRef } from "react";
 
 export default function ASG_5() {
- 
+  const [numbers, setNumbers] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef(null);
+
+  const addNumbers = () => {
+    const newNumber = parseFloat(inputValue);
+    if (!isNaN(newNumber)) {
+      const updateNnumbers = [...numbers, newNumber];
+      setNumbers(updateNnumbers);
+      console.log(updateNnumbers);
+      setInputValue("");
+      inputRef.current.focus();
+    }
+  };
+
+  const deleteNumber = (indexToRemove) => {
+    const updatedList = numbers.filter((_, index) => index !== indexToRemove);
+    setNumbers(updatedList);
+    console.log("updated List :", updatedList);
+    console.log("Deleted Index :",indexToRemove)
+  };
+
+  const sortAscending = () => {
+    const sortedList = [...numbers].sort((a, b) => a -b)
+    setNumbers(sortedList)
+  }
+
+  const sortDescending = () => {
+    const sortedList = [...numbers].sort((a, b) => b-a)
+    setNumbers(sortedList)
+
+  }
 
   return (
     <>
       <BackToHome />
       <h1 className="assignment-title">Assignment-5</h1>
       <hr />
+      <br />
+      <button className="btn-top" onClick={sortAscending}>Sort Ascending</button>
+      <button className="btn-top" onClick={sortDescending}>Sort Descending</button>
+
+      <ul className="number-list">
+        {numbers.map((num, index) => (
+          <li className="number-item" key={index}>
+            <span>{num}</span>
+            <button
+              className="delete-button"
+              onClick={() => deleteNumber(index)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+      <br />
+      <br />
+      <input
+        id="numInput"
+        type="number"
+        ref={inputRef}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      ></input>
+      <button
+        className="add-button"
+        onClick={addNumbers}
+        disabled={inputValue.trim() === ""}
+      >
+        Add
+      </button>
+      <label></label>
+      <br />
       <br />
     </>
   );
