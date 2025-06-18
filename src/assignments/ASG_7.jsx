846d3487b283d551639f307459f43089d5c91187
numeshj <numeshjayamanne@gmail.com>
2025-06-18 14:49:18 +0530

    Assignment #7-1

--- src/assignments/ASG_7.jsx
+++ src/assignments/ASG_7.jsx
@@ -1,9 +1,24 @@
 import BackToHome from "../component/BackToHome";
 import "../assignments/AGS_3.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function ASG_7() {
- 
+  const [color, setColor] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://apis.dnjs.lk/objects/colors.php");
+        setColor(response.data);
+        console.log("Data : ", response.data);
+      } catch (err) {
+        console.log("Error when fetch Data : ", err);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -11,6 +26,14 @@ export default function ASG_7() {
       <h1 className="assignment-title">Assignment-7</h1>
       <hr />
       <br />
+      <h2>Colors</h2>
+      <ul>
+        {color.map((item, index) => (
+          <li key={index}>
+            {item.name} - {item.code}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
