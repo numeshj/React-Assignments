b5007362cf92f8344cb82d9693fdb858f027df0e
numeshj <numeshjayamanne@gmail.com>
2025-06-27 18:41:07 +0530

    Assignment #17-3

--- src/assignments/ASG_18.jsx
+++ src/assignments/ASG_18.jsx
@@ -75,25 +75,15 @@ export default function ASG_18() {
   return (
     <div className="color-clicker-container">
       <BackToHome />
-      <h1 className="assignment-title">Color Clicker Game</h1>
+      <h1 className="assignment-title">
+        {showGameOver ? "Game Over!" : "Color Clicker Game"}
+      </h1>
       <div className="score-panel">
         <span className="score-label">Score:</span>{" "}
         <span className="score-value">{score}</span>
         <span
           className="score-icon"
-          style={{
-            display: 'inline-block',
-            width: 32,
-            height: 32,
-            marginLeft: 10,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'contain',
-            backgroundPosition: 'center',
-            verticalAlign: 'middle',
-            backgroundImage: !gameOver
-              ? `url('data:image/svg+xml,<svg version="1.1" viewBox="0 0 16 16" width="200" height="200" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="fill: rgb(240, 234, 66); opacity: 1; transform: rotate(0deg) scale(1, 1);"><g>    <g style="transform-origin: center center;" class="glyph_animate" index="1">                    <path d="M4.968 9.75a.5.5 0 1 0-.866.5A4.498 4.498 0 0 0 8 12.5a4.5 4.5 0 0 0 3.898-2.25.5.5 0 1 0-.866-.5A3.498 3.498 0 0 1 8 11.5a3.498 3.498 0 0 1-3.032-1.75zM7 5.116V5a1 1 0 0 0-1-1H3.28a1 1 0 0 0-.97 1.243l.311 1.242A2 2 0 0 0 4.561 8H5a2 2 0 0 0 1.994-1.839A2.99 2.99 0 0 1 8 6c.393 0 .74.064 1.006.161A2 2 0 0 0 11 8h.438a2 2 0 0 0 1.94-1.515l.311-1.242A1 1 0 0 0 12.72 4H10a1 1 0 0 0-1 1v.116A4.22 4.22 0 0 0 8 5c-.35 0-.69.04-1 .116z"></path><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-1 0A7 7 0 1 0 1 8a7 7 0 0 0 14 0z"></path>                                                                                        <animateTransform id="glyph_animate_element_1" attributeName="transform" type="scale" dur="1s" values="1;0.7;1" repeatCount="indefinite"></animateTransform>    </g>    </g></svg>')`
-              : `url('data:image/svg+xml,<svg version="1.1" viewBox="0 0 16 16" width="200" height="200" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="fill: rgb(240, 66, 66); opacity: 1; transform: rotate(0deg) scale(1, 1);"><g>    <g style="transform-origin: center center;" class="glyph_animate" index="1">                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path><path d="M4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm5 0a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"></path>                                                                                                                                        <animateTransform id="glyph_animate_element_1" attributeName="transform" type="scale" dur="1s" values="1;0.7;1" repeatCount="indefinite"></animateTransform>    </g>    </g></svg>')`,
-          }}
+          data-gameover={gameOver}
         ></span>
       </div>
       {!started && (
@@ -135,9 +125,9 @@ export default function ASG_18() {
       )}
       {showGameOver && (
         <div className="game-over-modal">
-          <div className="game-over-content">
-            <h2>Game Over!</h2>
-            <p>
+          <div className="game-over-content" style={{ boxShadow: "none", background: "none", padding: 0, textAlign: "center" }}>
+            {/* Empty div to keep modal overlay, but content is now in the main title area */}
+            <p style={{marginTop: 0}}>
               Your final score: <span className="final-score">{score}</span>
             </p>
             <button className="restart-btn" onClick={handleRestart}>
