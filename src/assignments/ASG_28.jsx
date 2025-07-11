1b81fab088507913c716fb728601010a81f15bb8
numeshj <numeshjayamanne@gmail.com>
2025-07-11 18:23:31 +0530

    Assignment #28-2

--- src/assignments/ASG_28.jsx
+++ src/assignments/ASG_28.jsx
@@ -1,15 +1,40 @@
 import BackToHome from "../component/BackToHome";
-import "../assignments/AGS_3.css";
+import "../assignments/ASG_28.css";
 import { useState } from "react";
 
 export default function ASG_28() {
+  const [tail, setTail] = useState([]);
+
+  const onMouseMove = (event) => {
+    const newDot = {
+      x: event.clientX,
+      y: event.clientY,
+      id: crypto.randomUUID(),
+    };
+
+    setTail((prev) => [...prev, newDot]);
+
+    setTimeout(() => {
+      setTail((prev) => prev.filter((dot) => dot.id !== newDot.id));
+    }, 300);
+  };
+
   return (
     <>
       <BackToHome />
       <h1 className="assignment-title">Assignment-28</h1>
       <hr />
       <br />
+      <div className="container" onMouseMove={onMouseMove}>
+        <div className="wording">MOVE YOUR CURSOR HERE</div>
+        {tail.map((dot) => (
+          <div
+            key={dot.id}
+            className="bubble"
+            style={{ left: dot.x + "px", top: dot.y + "px" }}
+          />
+        ))}
+      </div>
     </>
   );
 }
-
