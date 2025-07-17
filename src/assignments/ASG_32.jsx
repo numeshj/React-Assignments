c361b7738cd69216f62bf1afa8c729f9bf29d899
numeshj <numeshjayamanne@gmail.com>
2025-07-17 16:26:25 +0530

    Assignment #32-2

--- src/assignments/ASG_32.jsx
+++ src/assignments/ASG_32.jsx
@@ -4,12 +4,56 @@ import { useState } from "react";
 
 export default function ASG_32() {
   const [item, setItem] = useState([
-    "Item #1",
-    "Item #2",
-    "Item #3",
-    "Item #4",
-    "Item #5",
+    { text: "Item #1", color: "#FF6F61" },
+    { text: "Item #2", color: "#6B5B95" },
+    { text: "Item #3", color: "#88B04B" },
+    { text: "Item #4", color: "#F7CAC9" },
+    { text: "Item #5", color: "#92A8D1" },
   ]);
+  const [dropPosition, setDropPosition] = useState({
+    index: null,
+    position: null,
+  });
+
+  const handleDragStart = (event, index) => {
+    console.log("Draggin : ", index);
+    event.dataTransfer.setData("text/plain", index);
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+
+    const boundingReact = event.currentTarget.getBoundingReact();
+    const mouseY = event.clientY;
+
+    const elementTop = boundingReact.top;
+    const elementHeight = boundingReact.height;
+
+    const midlePoint = elementHeight + elementTop / 2;
+
+    if (mouseY < midlePoint) {
+      setDropPosition({ index: index, position: "top" });
+    } else {
+      setDropPosition({ index: index, position: "bottom" });
+    }
+  };
+
+  const handleDrop = (event, targetIndex) => {
+    event.preventDefault();
+
+    const draggedIndex = event.dataTransfer.getData("text/plain");
+
+    console.log("Dragged Item Index : ", draggedIndex);
+    console.log("Dropped on Item Index : ", targetIndex);
+
+    const updatedItems = [...item];
+
+    const draggedItem = updatedItems.splice(draggedIndex, 1)[0];
+
+    updatedItems.splice(targetIndex, 0, draggedItem);
+
+    setItem(updatedItems);
+  };
 
   return (
     <>
@@ -22,8 +66,16 @@ export default function ASG_32() {
         <div className="asg32-items-list" draggable="true">
           {item.map((item, index) => {
             return (
-              <div className="asg32-items" draggable="true" key={index}>
-                {item}
+              <div
+                className="asg32-items"
+                draggable="true"
+                key={index}
+                style={{ backgroundColor: item.color }}
+                onDragStart={(event) => handleDragStart(event, index)}
+                onDragOver={handleDragOver}
+                onDrop={(event) => handleDrop(event, index)}
+              >
+                {item.text}
               </div>
             );
           })}
