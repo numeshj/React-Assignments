a7c80d4bd31755297ab73c2ad59278990d461391
numeshj <numeshjayamanne@gmail.com>
2025-07-18 10:09:18 +0530

    Assignment #33-1

---
+++ src/assignments/ASG_34.jsx
@@ -0,0 +1,18 @@
+
+import BackToHome from "../component/BackToHome";
+import "../assignments/ASG_34.css";
+import { useState } from "react";
+
+export default function ASG_34() {
+  
+
+  return (
+    <>
+      <BackToHome />
+      <h1 className="assignment-title">Assignment-34</h1>
+      <hr />
+      <br />
+      
+    </>
+  );
+}
