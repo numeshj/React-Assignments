a7e7e0023426d3ec620ea46d5cc4f1a2b081feec
numeshj <numeshjayamanne@gmail.com>
2025-08-04 18:51:07 +0530

    Assignment #44-1

--- src/assignments/ASG_45.jsx
+++ src/assignments/ASG_45.jsx
@@ -1,14 +1,14 @@
 import BackToHome from "../component/BackToHome";
-import "../assignments/ASG_44.css";
+import "../assignments/ASG_45.css";
 import { useState, useEffect } from "react";
 
-export default function ASG_44() {
+export default function ASG_45() {
   
 
   return (
-    <div className="asg44">
+    <div className="asg45">
       <BackToHome />
-      <h1 className="assignment-title">Assignment-44</h1>
+      <h1 className="assignment-title">Assignment-45</h1>
       <hr />
       <br />
 
--- src/assignments/ASG_46.jsx
+++ src/assignments/ASG_46.jsx
@@ -1,14 +1,14 @@
 import BackToHome from "../component/BackToHome";
-import "../assignments/ASG_44.css";
+import "../assignments/ASG_46.css";
 import { useState, useEffect } from "react";
 
-export default function ASG_45() {
+export default function ASG_44() {
   
 
   return (
-    <div className="asg44">
+    <div className="asg46">
       <BackToHome />
-      <h1 className="assignment-title">Assignment-44</h1>
+      <h1 className="assignment-title">Assignment-46</h1>
       <hr />
       <br />
 
