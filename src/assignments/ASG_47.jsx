fc9014a67b647b889d93794cf60a2e1ace3448f5
numeshj <numeshjayamanne@gmail.com>
2025-08-05 20:47:05 +0530

    Assignment #46-1

--- src/assignments/ASG_47.jsx
+++ src/assignments/ASG_47.jsx
@@ -1,14 +1,14 @@
 import BackToHome from "../component/BackToHome";
-import "../assignments/ASG_46.css";
+import "../assignments/ASG_47.css";
 import { useState, useEffect } from "react";
 
-export default function ASG_46() {
+export default function ASG_47() {
   
 
   return (
-    <div className="asg46">
+    <div className="asg47">
       <BackToHome />
-      <h1 className="assignment-title">Assignment-46</h1>
+      <h1 className="assignment-title">Assignment-47</h1>
       <hr />
       <br />
 
