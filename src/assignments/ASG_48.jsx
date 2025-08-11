d2fbe6a5669207ca7a0d46ba45b9d8245a294f5c
numeshj <numeshjayamanne@gmail.com>
2025-08-11 15:41:51 +0530

    Assignment 48-2

--- src/assignments/ASG_48.jsx
+++ src/assignments/ASG_48.jsx
@@ -1,9 +1,33 @@
 import BackToHome from "../component/BackToHome";
 import "../assignments/ASG_48.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+
+
 
 export default function ASG_48() {
-  
+
+  const ROWS = 13;
+  const COLS = 13;
+
+  const grid = [];
+  for (let y = 0; y < ROWS; y++) {
+    const row = [];
+    for (let x = 0; x < COLS; x++) {
+      row.push(
+        <div
+          key={x}
+          className="board-asg48-cell"
+          data-x={x}
+          data-y={y}
+        />
+      );
+    }
+    grid.push(
+      <div className="board-asg48-row"
+        key={y}>{row}</div>
+    );
+  }
+
 
   return (
     <div className="asg48">
@@ -11,8 +35,10 @@ export default function ASG_48() {
       <h1 className="assignment-title">Assignment-48</h1>
       <hr />
       <br />
-
+      <div className="container-asg48">
+        <div className="board-asg48">{grid}</div>
+        <div className="hint-asg48">Use W, A, S, D, or Arrow Keys</div>
+      </div>
     </div>
   );
 }
-            
