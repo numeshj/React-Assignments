48a23e12c62a9969c79c5fb23ebc1aabe664e853
numeshj <numeshjayamanne@gmail.com>
2025-08-18 16:51:15 +0530

    Assignment #54-3

--- src/assignments/ASG_54.jsx
+++ src/assignments/ASG_54.jsx
@@ -57,6 +57,23 @@ export default function ASG_54() {
     setHistoryIndex(-1)
   }
 
+  const getColor = (item) => {
+    if (item.isError) return "red";
+    switch (item.type) {
+      case "string":
+        return "#fe8d59";
+      case "number":
+      case "boolean":
+        return "#9980ff";
+      case "object":
+        return "#5cd5fb";
+      case "undefined":
+        return "#888888";
+      default:
+        return "#ffffff";
+    }
+  }
+
 
   return (
     <div className="asg54">
@@ -64,6 +81,37 @@ export default function ASG_54() {
       <h1 className="assignment-title">Assignment-54</h1>
       <hr />
       <br />
+
+      <div className="container-asg54">
+        <div className="outputs-asg54">
+          <div className="listen-asg54" ref={outputRef}>
+            {history.length === 0 ? (
+              <div className="center-asg54">Outputs will be displayed here</div>
+            ) : (
+                history.map((item, index) => (
+                  <>
+                    <div className="item-asg54" key={index}>
+                      <span>&gt; {item.cmd}</span>
+                    </div>
+                    <div className="item-command-asg54" style={{color: getColor(item)}}>
+                        {string(item.output)}
+                    </div>
+                  </>
+                ))
+            )}
+          </div>
+        </div>
+        <input
+          type="text"
+          className="input-asg54"
+          spellCheck="false"
+          placeholder="Input your JavaScript command and hit enter"
+          value={command}
+          onChange={(e) => setCommand(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+      </div>
+
     </div>
   );
 }
