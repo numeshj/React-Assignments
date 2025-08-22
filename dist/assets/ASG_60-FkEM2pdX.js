76bb5d2445de5175e14517095a19886ef0bcfe7d
numeshj <numeshjayamanne@gmail.com>
2025-08-22 12:41:59 +0530

    Assignment #58-2

--- dist/assets/ASG_60-FkEM2pdX.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Cgsh1-IF.js";import{B as r}from"./BackToHome-BOs2wqmu.js";/* empty css               *//* empty css                   */function a(){return s.jsxs("div",{className:"asg59",children:[s.jsx(r,{}),s.jsx("h1",{className:"assignment-title",children:"Assignment-59"}),s.jsx("hr",{}),s.jsx("br",{})]})}export{a as default};
--- src/assignments/ASG_58.jsx
+++ src/assignments/ASG_58.jsx
@@ -5,40 +5,111 @@ export default function ASG_58() {
   return (
     <div className="asg58">
       <BackToHome />
-      
+
       <div className="container-asg58">
         <div className="editor-asg58">
+          {/* Listing items */}
           <div className="listing-asg58">
-            <div className="listing-asg58-item" data-active="true" style={{ background: "" }}></div>
-            <div className="listing-asg58-item" data-active="true" style={{ background: "" }}></div>
-            <div className="listing-asg58-item" data-active="true" style={{ background: "" }}></div>
-            <div className="listing-asg58-item" data-active="true" style={{ background: "" }}></div>
-            <div className="listing-asg58-item" data-active="true" style={{ background: "" }}></div>
-            <div className="listing-asg58-item-add"></div>
+            <div
+              className="listing-asg58-item"
+              data-active="true"
+              style={{ background: "rgb(51, 102, 204)" }}
+            >
+              T
+            </div>
+            <div
+              className="listing-asg58-item"
+              data-active="false"
+              style={{ background: "rgb(204, 0, 68)" }}
+            >
+              A
+            </div>
+            <div
+              className="listing-asg58-item"
+              data-active="false"
+              style={{ background: "rgb(45, 134, 89)" }}
+            >
+              O
+            </div>
+            <div
+              className="listing-asg58-item"
+              data-active="false"
+              style={{ background: "rgb(219, 94, 10)" }}
+            >
+              C
+            </div>
+            <div
+              className="listing-asg58-item"
+              data-active="false"
+              style={{ background: "rgb(138, 0, 230)" }}
+            >
+              R
+            </div>
+            <div className="listing-asg58-item listing-asg58-item-add"></div>
+          </div>
+
+          {/* Slide */}
+          <div className="slide-asg58" style={{ background: "rgb(51, 102, 204)" }}>
+            <input
+              className="slide-asg58-input"
+              spellCheck="false"
+              value="THIS IS"
+              readOnly
+            />
+            <div className="background-asg58">
+              <div
+                className="background-item-asg58"
+                data-active="true"
+                style={{ background: "rgb(51, 102, 204)" }}
+              ></div>
+              <div
+                className="background-item-asg58"
+                data-active="false"
+                style={{ background: "rgb(204, 0, 68)" }}
+              ></div>
+              <div
+                className="background-item-asg58"
+                data-active="false"
+                style={{ background: "rgb(45, 134, 89)" }}
+              ></div>
+              <div
+                className="background-item-asg58"
+                data-active="false"
+                style={{ background: "rgb(219, 94, 10)" }}
+              ></div>
+              <div
+                className="background-item-asg58"
+                data-active="false"
+                style={{ background: "rgb(138, 0, 230)" }}
+              ></div>
+            </div>
+
+            {/* Animations */}
+            <div className="animations-asg58">
+              <div className="animation-item-asg58" data-active="false">
+                Instant
+              </div>
+              <div className="animation-item-asg58" data-active="false">
+                Fade
+              </div>
+              <div className="animation-item-asg58" data-active="true">
+                Up
+              </div>
+              <div className="animation-item-asg58" data-active="false">
+                Down
+              </div>
+              <div className="animation-item-asg58" data-active="false">
+                Blur
+              </div>
+              <div className="animation-item-asg58" data-active="false">
+                Rotate
+              </div>
+            </div>
+
+            <div className="start-asg58"></div>
+            <div className="delete-asg58"></div>
           </div>
         </div>
-
-        <div className="slide-asg58" style={{background: ""}}>
-          <input spellCheck="false" value="THIS IS" />
-          <div className="background-asg58">
-            <div className="background-item-asg58" data-active="true" style={{ background: "rgb(51, 102, 204)" }}></div>
-            <div className="background-item-asg58" data-active="false" style={{ background: "rgb(204, 0, 68)" }}></div>
-            <div className="background-item-asg58" data-active="false" style={{ background: "rgb(45, 134, 89)" }}></div>
-            <div className="background-item-asg58" data-active="false" style={{ background: "rgb(219, 94, 10)" }}></div>
-            <div className="background-item-asg58" data-active="false" style={{ background: "rgb(138, 0, 230)" }}></div>
-          </div>
-        </div>
-        <div className="animations-asg58">
-          <div className="animation-item-asg58" data-active="false">Instant</div>
-          <div className="animation-item-asg58" data-active="false">Fade</div>
-          <div className="animation-item-asg58" data-active="true">Up</div>
-          <div className="animation-item-asg58" data-active="false">Down</div>
-          <div className="animation-item-asg58" data-active="false">Blur</div>
-          <div className="animation-item-asg58" data-active="false">Rotate</div>
-        </div>
-        <div className="start-asg58"></div>
-        <div className="delete-asg58"></div>
-
       </div>
 
       <hr />
